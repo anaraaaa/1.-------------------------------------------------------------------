0c26d41910c82229496c4b311ff989cdf1caf085
anaraaaa <anara_1502@mail.ru>
2022-05-05 13:02:56 +0600

    first commit

---
+++ astansite/contact.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Контакты</title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+  <div id="header">
+    <!-- Верхняя часть страницы -->
+    <img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
+    <span class="slogan">приходите к нам учиться</span>
+    <!-- Верхняя часть страницы -->
+  </div>
+
+  <div id="content">
+    <!-- Заголовок -->
+    <h1>Обратная связь</h1>
+    <!-- Заголовок -->
+    <!-- Область основного контента -->
+            <?php
+    $a = (0);
+    $size = 8;
+        switch ($size) {
+ case 0:
+ $a = (0); break;
+ case 1:
+ $a = (1); break;
+ case 2:
+ $a = (2); break;
+ case 3:
+ $a = (3); break;
+ case 4:
+ $a = (4); break;
+ case 5:
+ $a = (5); break;
+ case 6:
+ $a = (6); break;
+ case 7:
+ $a = (7); break;
+ case 8: 
+ $a = (8); break;
+ 
+}
+
+?>
+    <h3>Адрес</h3>
+    <p>123456 Москва, Малый Американский переулок 21</p>
+    <h3>Задайте вопрос</h3>
+    <form action='' method='post'>
+      <label>Тема письма: </label>
+      <br />
+      <input name='subject' type='text' size="50" />
+      <br />
+      <label>Содержание: </label>
+      <br />
+      <textarea name='body' cols="50" rows="10"></textarea>
+      <br />
+      <br />
+      <input type='submit' value='Отправить' />
+    </form>
+      <p>Максимальный размер отправляемых данных <?= $a ?> мегабайт.</p>
+    <!-- Область основного контента -->
+  </div>
+  <div id="nav">
+    <h2>Навигация по сайту</h2>
+    <!-- Меню -->
+    <ul>
+      <li><a href='index.php'>Домой</a>
+      </li>
+      <li><a href='about.php'>О нас</a>
+      </li>
+      <li><a href='contact.php'>Контакты</a>
+      </li>
+      <li><a href='table.php'>Таблица умножения</a>
+      </li>
+      <li><a href='calc.php'>Калькулятор</a>
+      </li>
+    </ul>
+    <!-- Меню -->
+  </div>
+  <div id="footer">
+    <!-- Нижняя часть страницы -->
+    &copy; Супер Мега Веб-мастер, 2000 &ndash; 2015
+    <!-- Нижняя часть страницы -->
+  </div>
+</body>
+
+</html>
